d75f922036cd4057ad2817089913fdc0491c9770
jiangke <jiangkeredgirl@hotmail.com>
2018-05-05 16:39:11 +0800

    重构tcpclientsocket

--- LibNetwork/tcpclientsocket.cpp
+++ LibNetwork/tcpclientsocket.cpp
@@ -1,6 +1,6 @@
-#include "tcpclientsocket.h"
+Ôªø#include "tcpclientsocket.h"
 #include <boost/bind.hpp>
-
+#include "kloglib.h"
 
 CTcpClientSocket::CTcpClientSocket() : m_ioservice(), m_socket(m_ioservice)
 {
@@ -26,180 +26,321 @@ int CTcpClientSocket::RegisterHandler(f_connect connect_callback, f_disconnect d
 	return 0;
 }
 
+int CTcpClientSocket::SetReconnectTime(__int64 reconnect_second)
+{
+	m_reconnect_second = reconnect_second;
+	return 0;
+}
+
 int CTcpClientSocket::Connect(const string& ip, int port)
 {
-	m_thread_client = thread(bind(&CTcpClientSocket::SocketClientRunThread, this, false));
 	ip::tcp::endpoint ep(boost::asio::ip::address_v4::from_string(ip), port);
 	boost::system::error_code ec;
 	m_socket.connect(ep, ec);
 	if (ec)
 	{
-		Disconnect();
+		TraceErrorCout() << "tcp client sync connect error, error code:" << ec.value() << ", error message:" << ec.message();
+		m_socket.close();
+		//Disconnect(ec.value());
 	}
-	cout << "tcp client connect, error code:" << ec.value() << ", error message:" << ec.message() <<  endl;
+	else
+	{
+		TraceOKCout() << "tcp client sync connect success";
+		m_keep_connect = true;
+		m_sync_connect = true;
+		m_thread_client = thread(bind(&CTcpClientSocket::SocketClientRunThread, this, false));
+	}
 	return ec.value();
 }
 
 int CTcpClientSocket::AsyncConnect(const string& ip, int port)
 {
 	ip::tcp::endpoint ep(boost::asio::ip::address_v4::from_string(ip), port);
-	m_socket.async_connect(ep, std::bind(&CTcpClientSocket::ConnectHandler, this, std::placeholders::_1));
+	m_socket.async_connect(ep, [this](boost::system::error_code ec)
+	{
+		if (m_connect_callback)
+		{
+			m_connect_callback(ec.value());
+		}
+		if (ec)
+		{
+			TraceErrorCout() << "tcp client async connect error, error code:" << ec.value() << ", error message:" << ec.message();
+			m_socket.close();
+			//Disconnect(ec.value());
+		}
+		else
+		{
+			TraceOKCout() << "tcp client async connect success";
+			m_keep_connect = true;
+			m_sync_connect = false;
+			ReadHeader();
+		}
+	});
 	m_thread_client = thread(bind(&CTcpClientSocket::SocketClientRunThread, this, true));
 	return 0;
 }
 
 int CTcpClientSocket::Disconnect()
 {
-	//cout << "tcp client disconected, server ip:" << m_socket.remote_endpoint().address().to_string() << endl;
-	cout << "tcp client socket closed" << endl;
+	TrackCout();
+	TraceInfoCout() << "tcp client disconnect, server ip:" << m_socket.remote_endpoint().address().to_string();
+	TraceInfoCout() << "tcp client socket will close";
 	boost::system::error_code ec;
+	m_keep_connect = false;
 	if (m_socket.is_open())
 	{
 		m_socket.shutdown(boost::asio::ip::tcp::socket::shutdown_both, ec);
 		m_socket.close(ec); // excute this statement occure crash
 	}
-	cout << "tcp client disconnected" << endl;
+	TraceInfoCout() << "tcp client ioservice will stop";
 	m_ioservice.stop();
 	if (m_thread_client.joinable())
 	{
-		cout << "waiting SocketClientRunThread end" << endl;
+		TraceInfoCout() << "waiting SocketClientRunThread over";
 		m_thread_client.join();
+	}
+	if (m_disconnect_callback)
+	{
+		m_disconnect_callback(ec.value());
 	}
 	return 0;
 }
 
 int CTcpClientSocket::Write(const char* data, size_t size)
 {
-	boost::system::error_code ec;
-	size_t write_size = 0;
-	if(m_socket.is_open())
+	int error_code = 1;
+	if (m_socket.is_open() && data && size > 0)
 	{
-		write_size = m_socket.write_some(buffer(data, size), ec);
+		boost::system::error_code ec;
+		size_t writed_size = 0;
+		NetDataPackage write_package(data, size);
+		writed_size = boost::asio::write(m_socket, buffer(write_package.data(), size), ec);
+		error_code = WriteErrorCheckHandler(ec, writed_size, size);
 	}
-	return ec.value();
+	return error_code;
 }
 
 int CTcpClientSocket::AsyncWrite(const char* data, size_t size)
 {
-	if (m_socket.is_open())
+	if (m_socket.is_open() && data && size > 0)
 	{
-		memset(m_writebuffer, 0, sizeof(m_writebuffer));
-		memcpy(m_writebuffer, data, size);
-		m_socket.async_write_some(buffer(m_writebuffer, size), bind(&CTcpClientSocket::WriteHandler, this, std::placeholders::_1, std::placeholders::_2));
+		shared_ptr<NetDataPackage> write_package = shared_ptr<NetDataPackage>(new NetDataPackage(data, size));
+		m_write_packages.push_back(write_package);
+		DoWrite();
 	}
 	return 0;
 }
 
 int CTcpClientSocket::SocketClientRunThread(bool async)
 {
+	TrackCout();
 	if (async)
 	{
-		cout << "tcp client ioservice runing" << endl;
+		TraceInfoCout() << "async tcp client ioservice runing";
 		m_ioservice.run();
-		cout << "tcp client ioservice run over" << endl;
+		TraceInfoCout() << "async tcp client ioservice run over";
 	}
 	else
 	{
-		boost::system::error_code ec;
-		char readbuffer[MAXBUFFER];
 		while (true)
 		{
-			memset(readbuffer, 0, sizeof(readbuffer));
-			size_t bytes_readed = m_socket.read_some(boost::asio::buffer(readbuffer), ec);
-			cout << "tcp client read:" << readbuffer << ", error code:" << ec.value() << ", error message:" << ec.message() << endl;
-			if (ec == boost::asio::error::eof)
+			if (!m_keep_connect)
 			{
 				break;
 			}
-			else if (ec)
-			{
-				std::thread th = std::thread([this](){Disconnect(); });
-				th.detach();
-				break;
-			}
-			else
+			boost::system::error_code ec;
+			m_read_package = shared_ptr<NetDataPackage>(new NetDataPackage());
+			size_t readed_size = boost::asio::read(m_socket, boost::asio::buffer(m_read_package->header(), NetDataPackage::HEADER_SIZE), ec);
+			int error_code = ReadErrorCheckHandler(ec, readed_size, NetDataPackage::HEADER_SIZE);
+			if (error_code)
 			{
 				if (m_read_callback)
 				{
-					m_read_callback(readbuffer, bytes_readed, ec.value());
+					m_read_callback(nullptr, 0, error_code);
 				}
+				continue;
 			}
+			if (m_read_package->decode_header())
+			{
+				TraceErrorCout() << "tcp client decode header error";
+				if (m_read_callback)
+				{
+					m_read_callback(nullptr, 0, error_code);
+				}
+				continue;
+			}
+
+			readed_size = boost::asio::read(m_socket, boost::asio::buffer(m_read_package->body(), m_read_package->header()->body_size), ec);
+			error_code = ReadErrorCheckHandler(ec, readed_size, m_read_package->header()->body_size);
+			if (error_code)
+			{
+				if (m_read_callback)
+				{
+					m_read_callback(nullptr, 0, error_code);
+				}
+				continue;
+			}
+			if (m_read_callback)
+			{
+				(TraceTempCout() << "tcp client readed payload data size=" << m_read_package->header()->body_size << ", payload data:").write(m_read_package->body(), m_read_package->header()->body_size);
+				m_read_callback(m_read_package->body(), readed_size, ec.value());
+			}
+
 		}
 	}
 	return 0;
 }
+ 
+//void CTcpClientSocket::DisconnectHandler()
+//{
+//	m_ioservice.stop();
+//	// ¬≤¬ª√í¬™¬µ√à√è√ü¬≥√å¬Ω√°√ä√∏¬ª√°√ó√®√à√ª
+//	//if (m_thread_client.joinable())
+//	//{
+//	//	cout << "waiting SocketClientRunThread end" << endl;
+//	//	m_thread_client.join();
+//	//}
+//	if (m_disconnect_callback)
+//	{
+//		m_disconnect_callback(0);
+//	}
+//}
 
-void CTcpClientSocket::ConnectHandler(const boost::system::error_code& ec)
+void CTcpClientSocket::DoWrite()
 {
-	cout << "tcp client connect, error code:" << ec.value() << ", error message:" << ec.message() << endl;
-	if(ec)
+	if (!m_write_packages.empty())
 	{
-		//Disconnect();
-		DisconnectHandler();
-	}
-	else
-	{
-		if (m_connect_callback)
+		boost::asio::async_write(m_socket, boost::asio::buffer(m_write_packages.front()->data(),
+			m_write_packages.front()->data_size()),
+			[this](boost::system::error_code ec, std::size_t length)
 		{
-			m_connect_callback(ec.value());
+			int error_code = WriteErrorCheckHandler(ec, length, m_write_packages.front()->data_size());
+			if (error_code)
+			{
+				if (m_write_callback)
+				{
+					m_write_callback(nullptr, 0, error_code);
+				}
+			}
+			else
+			{
+				if (m_write_callback)
+				{
+					(TraceTempCout() << "tcp client writed payload data size=" << m_write_packages.front()->header()->body_size << ", payload data:").write(m_write_packages.front()->body(), m_write_packages.front()->header()->body_size);
+					m_write_callback(m_write_packages.front()->body(), m_write_packages.front()->header()->body_size, ec.value());
+				}
+				m_write_packages.pop_front();
+				if (!m_write_packages.empty())
+				{
+					DoWrite();
+				}
+			}
+		});
+	}
+}
+
+void CTcpClientSocket::ReadHeader()
+{
+	m_read_package = shared_ptr<NetDataPackage>(new NetDataPackage());
+	boost::asio::async_read(m_socket,
+		boost::asio::buffer(m_read_package->header(), NetDataPackage::HEADER_SIZE),
+		[this](boost::system::error_code ec, std::size_t length)
+	{
+		int error_code = ReadErrorCheckHandler(ec, length, NetDataPackage::HEADER_SIZE);
+		if (error_code)
+		{
+			if (m_read_callback)
+			{
+				m_read_callback(nullptr, 0, error_code);
+			}
 		}
-		memset(m_readbuffer, 0, sizeof(m_readbuffer));
-		m_socket.async_read_some(buffer(m_readbuffer, MAXBUFFER), std::bind(&CTcpClientSocket::ReadHandler, this, std::placeholders::_1, std::placeholders::_2));
-	}
-}
-
-void CTcpClientSocket::DisconnectHandler()
-{
-	m_ioservice.stop();
-	// ≤ª“™µ»œﬂ≥ÃΩ· ¯ª·◊Ë»˚
-	//if (m_thread_client.joinable())
-	//{
-	//	cout << "waiting SocketClientRunThread end" << endl;
-	//	m_thread_client.join();
-	//}
-	if (m_disconnect_callback)
-	{
-		m_disconnect_callback(0);
-	}
-}
-
-void CTcpClientSocket::ReadHandler(boost::system::error_code ec, size_t bytes_readed)
-{
-	cout << "tcp client read error code:" << ec.value() << ", error message:" << ec.message() << endl;
-	cout << "read buffer:" << endl;
-	cout.write(m_readbuffer, bytes_readed) << endl;
-	if (ec)	
-	{
-		//Disconnect();
-		DisconnectHandler();
-	}
-	else
-	{
-		if (m_read_callback)
+		else if (m_read_package->decode_header())
 		{
-			m_read_callback(m_readbuffer, bytes_readed, ec.value());
+			TraceErrorCout() << "tcp client decode header error";
+			if (m_read_callback)
+			{
+				m_read_callback(nullptr, 0, error_code);
+			}
 		}
-		memset(m_readbuffer, 0, sizeof(m_readbuffer));
-		m_socket.async_read_some(buffer(m_readbuffer, MAXBUFFER), std::bind(&CTcpClientSocket::ReadHandler, this, std::placeholders::_1, std::placeholders::_2));
-	}
+		else
+		{
+			ReadBody();
+		}
+	});
 }
 
-void CTcpClientSocket::WriteHandler(boost::system::error_code ec, size_t bytes_writed)
+void CTcpClientSocket::ReadBody()
 {
-	cout << "tcp client write, error code:" << ec.value() << ", error message:" << ec.message() << endl;
-	cout << "write buffer:" << endl;
-	cout.write(m_writebuffer, bytes_writed) << endl;
-	if (ec)	
+	boost::asio::async_read(m_socket,
+		boost::asio::buffer(m_read_package->body(), m_read_package->header()->body_size),
+		[this](boost::system::error_code ec, std::size_t length)
 	{
-		//Disconnect();
-		DisconnectHandler();
-	}
-	else
-	{
-		if (m_write_callback)
+		int error_code = ReadErrorCheckHandler(ec, length, NetDataPackage::HEADER_SIZE);
+		if (error_code)
 		{
-			m_write_callback(m_writebuffer, bytes_writed, ec.value());
-		}	
-		memset(m_writebuffer, 0, sizeof(m_writebuffer));
-	}	
+			if (m_read_callback)
+			{
+				m_read_callback(nullptr, 0, error_code);
+			}
+		}
+		else
+		{
+			//std::cout.write(m_read_package->body(), m_read_package->header()->body_size);
+			if (m_read_callback)
+			{
+				(TraceTempCout() << "tcp client readed payload data size=" << m_read_package->header()->body_size << ", payload data:").write(m_read_package->body(), m_read_package->header()->body_size);
+				m_read_callback(m_read_package->body(), m_read_package->header()->body_size, ec.value());
+			}
+			ReadHeader();
+		}
+	});
+}
+
+int CTcpClientSocket::ReadErrorCheckHandler(boost::system::error_code ec, size_t readed_size, size_t require_read_size)
+{
+	int error_code = 1;
+	do
+	{
+		if (ec == boost::asio::error::eof)
+		{
+			TraceInfoCout() << "tcp client read eof";
+			break;
+		}
+		if (ec)
+		{
+			TraceErrorCout() << "tcp client read error, error code:" << ec.value() << ", error message:" << ec.message();
+			//std::thread th = std::thread([this, ec]() {Disconnect(ec.value()); });
+			//th.detach();
+			break;
+		}
+		if (readed_size != require_read_size)
+		{
+			TraceErrorCout() << "tcp client read error, readed_size != require_read_size, readed_size=" << readed_size << ", require_read_size=" << require_read_size;
+			break;
+		}
+		error_code = 0;
+	} while (false);
+	return error_code;
+}
+
+int CTcpClientSocket::WriteErrorCheckHandler(boost::system::error_code ec, size_t writed_size, size_t require_write_size)
+{
+	int error_code = 1;
+	do
+	{		
+		if (ec)
+		{
+			TraceErrorCout() << "tcp client write error, error code:" << ec.value() << ", error message:" << ec.message();
+			//std::thread th = std::thread([this, ec]() {Disconnect(ec.value()); });
+			//th.detach();
+			break;
+		}
+		if (writed_size != require_write_size)
+		{
+			TraceErrorCout() << "tcp client write error, writed_size != require_write_size, writed_size=" << writed_size << ", require_write_size=" << require_write_size;
+			break;
+		}
+		error_code = 0;
+	} while (false);
+	return error_code;
 }
--- LibNetwork/tcpclientsocket.h
+++ LibNetwork/tcpclientsocket.h
@@ -2,6 +2,7 @@
 
 #include "kutility.h"
 #include <boost/asio.hpp>
+#include "NetDataPackage.h"
 
 using namespace boost::asio;
 using ip::tcp;
@@ -10,6 +11,7 @@ typedef function<int (int status)> f_connect;
 typedef function<int (int status)> f_disconnect;
 typedef function<int(const char* data, size_t size, int status)> f_read;
 typedef function<int(const char* data, size_t size, int status)> f_write;
+
 
 class CTcpClientSocket : public enable_shared_from_this<CTcpClientSocket>
 {
@@ -21,6 +23,7 @@ public:
 	static CTcpClientSocket& instance();
 public:
 	int RegisterHandler(f_connect connect_callback = nullptr, f_disconnect disconnect_callback = nullptr, f_read read_callback = nullptr, f_write write_callback = nullptr);
+	int SetReconnectTime(__int64 reconnect_second);
 	int Connect(const string& ip, int port);
 	int AsyncConnect(const string& ip, int port);
 	int Disconnect();
@@ -29,11 +32,15 @@ public:
 
 private:
 	int  SocketClientRunThread(bool async);
-	void ConnectHandler(const boost::system::error_code& error);
-	void DisconnectHandler();
-	void ReadHandler(boost::system::error_code err_code, size_t bytes_readed);
-	void WriteHandler(boost::system::error_code err_code, size_t bytes_writed);
+	void DoWrite();	
+	void ReadHeader();
+	void ReadBody();
+	int ReadErrorCheckHandler(boost::system::error_code ec, size_t readed_size, size_t require_read_size);
+	int WriteErrorCheckHandler(boost::system::error_code ec, size_t writed_size, size_t require_write_size);
+	//int Reconnect();
+	//int DoConnect();
 
+private:
 	f_connect          m_connect_callback;
 	f_disconnect       m_disconnect_callback;
 	f_read             m_read_callback;
@@ -46,8 +53,11 @@ private:
 	io_service         m_ioservice;
 	tcp::socket        m_socket;
 
-	enum { MAXBUFFER = 1024 };
-	char m_readbuffer[MAXBUFFER];
-	char m_writebuffer[MAXBUFFER];
+	//enum { MAXBUFFER = 1024 };
+	shared_ptr<NetDataPackage> m_read_package;
+	list<shared_ptr<NetDataPackage>> m_write_packages;
+	__int64 m_reconnect_second = 0;
+	bool m_keep_connect = false;
+	bool m_sync_connect = false;
 };
 
