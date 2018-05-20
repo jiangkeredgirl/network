d911880db28db88a662f0e589d425f796e9b90b6
jiangke <jiangkeredgirl@hotmail.com>
2018-05-20 11:48:18 +0800

    更新klogsdk

--- NetworkServer/PostBuildEvent.bat
+++ NetworkServer/PostBuildEvent.bat
@@ -3,3 +3,5 @@ echo f|xcopy /y  .\bin\NetworkServer*.exe                  .\OutNetworkServerBin
 echo f|xcopy /y  .\bin\NetworkServer*.pdb                  .\OutNetworkServerBin\
 echo f|xcopy /y /s /i /f .\NetworkSDK\bin\*                .\OutNetworkServerBin\
 echo f|xcopy /y /s /i /f .\NetworkSDK\bin\*                .\bin\
+echo f|xcopy /y /s /i /f ..\KlogSDK\bin\*                  .\OutNetworkServerBin\
+echo f|xcopy /y /s /i /f ..\KlogSDK\bin\*                  .\bin\
