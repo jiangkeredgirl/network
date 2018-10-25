6244d153e89015934f0acef6f6113b2bc165ddaa
jiangke <jiangkeredgirl@hotmail.com>
2018-10-25 23:02:40 +0800

    替换boost库

--- LibNetwork/PostBuildEvent.bat
+++ LibNetwork/PostBuildEvent.bat
@@ -1,4 +1,5 @@
 echo copy to out sdk package
+set toolset=%1
 echo f|xcopy /y  .\tcpclient.h                     .\OutNetworkSDK\include\
 echo f|xcopy /y  .\tcpclienthandler.h              .\OutNetworkSDK\include\
 echo f|xcopy /y  .\tcpserver.h                     .\OutNetworkSDK\include\
@@ -7,3 +8,6 @@ echo f|xcopy /y  .\TcpPackage.h                    .\OutNetworkSDK\include\
 echo f|xcopy /y  .\bin\LibNetwork*.lib             .\OutNetworkSDK\lib\
 echo f|xcopy /y  .\bin\LibNetwork*.dll             .\OutNetworkSDK\bin\
 echo f|xcopy /y  .\bin\LibNetwork*.pdb             .\OutNetworkSDK\bin\
+echo f|xcopy /y  ..\..\thirdparty\boost\lib\msvc-%toolset%\boost_system*.dll          .\OutNetworkSDK\bin\
+echo f|xcopy /y  ..\..\thirdparty\boost\lib\msvc-%toolset%\boost_date_time*.dll       .\OutNetworkSDK\bin\
+echo f|xcopy /y  ..\..\thirdparty\boost\lib\msvc-%toolset%\boost_regex*.dll           .\OutNetworkSDK\bin\
